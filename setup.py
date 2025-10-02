7ff9d1e0643a833758f1d7716f1c5f29c8d11913
Jayesh <jgadhari5@gmail.com>
2025-10-02 19:58:58 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,11 @@
+
+from setuptools import find_packages, setup
+
+setup(
+    name="AI_Medical_Chatbot",
+    version="0.1.0",
+    author="Jayesh Gadhari",
+    author_email="jgadhari5@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
